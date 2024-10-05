7f96b32df8e5a231d1cd67c5cc71897ee5c739f8
Shrey Singhal <shreynbd12@gmail.com>
2024-10-05 19:11:15 +0530

    Modified: routers/adminRoute.js file

--- routers/adminRoute.js
+++ routers/adminRoute.js
@@ -8,5 +8,4 @@ const adminRouter = express.Router();
 adminRouter.post("/add-doctor", authAdmin, upload.single("image"), addDoctor);
 adminRouter.post("/login", loginAdmin);
 
-
 export default adminRouter;
