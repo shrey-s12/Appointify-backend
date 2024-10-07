94758c2b17e36706d6845cab19afba7bf5bd94bb
Shrey Singhal <shreynbd12@gmail.com>
2024-10-07 19:32:55 +0530

    Create a Doctor login Controller

--- controllers/doctorController.js
+++ controllers/doctorController.js
@@ -1,17 +1,16 @@
 import doctorModel from "../models/doctorModel.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 
 const changeAvailability = async (req, res) => {
     try {
-
         const { docId } = req.body;
-
         const docData = await doctorModel.findById(docId);
         await doctorModel.findByIdAndUpdate(docId, { available: !docData.available });
         res.json({ success: true, message: "Availability changed successfully" });
-
     } catch (error) {
-        res.status(500).json({ success: false, message: error.message });
+        res.json({ success: false, message: error.message });
     }
 }
 
@@ -20,8 +19,35 @@ const doctorList = async (req, res) => {
         const doctors = await doctorModel.find({}).select(["-password", "-email"]);
         res.json({ success: true, doctors });
     } catch (error) {
-        res.status(500).json({ success: false, message: error.message });
+        res.json({ success: false, message: error.message });
     }
 }
 
-export { changeAvailability, doctorList }
+// API for Doctor Login
+const loginDoctor = async (req, res) => {
+
+    try {
+
+        const { email, password } = req.body;
+        const doctor = await doctorModel.findOne({ email })
+
+        if (!doctor) {
+            return res.json({ success: false, message: "Invalid Credentials" });
+        }
+
+        const isMatch = await bcrypt.compare(password, doctor.password);
+
+        if (isMatch) {
+            const token = jwt.sign({ id: doctor._id }, process.env.JWT_SECRET);
+            return res.json({ success: true, token });
+        }
+        else {
+            return res.json({ success: false, message: "Invalid Credentials" });
+        }
+
+    } catch (error) {
+        return res.json({ success: false, message: error.message });
+    }
+}
+
+export { changeAvailability, doctorList, loginDoctor }
