fef7a95b066a5f157dd28c6ccafea625ed0ab856
Shrey Singhal <shreynbd12@gmail.com>
2024-10-07 21:15:10 +0530

    Create appointmentsDoctor controller and authDoctor Middleware

--- controllers/doctorController.js
+++ controllers/doctorController.js
@@ -1,6 +1,7 @@
 import doctorModel from "../models/doctorModel.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import appointmentModel from "../models/appointmentModel.js";
 
 
 const changeAvailability = async (req, res) => {
@@ -50,4 +51,18 @@ const loginDoctor = async (req, res) => {
     }
 }
 
-export { changeAvailability, doctorList, loginDoctor }
+// API to get doctor appointments for doctor panel
+const appointmentsDoctor = async (req, res) => {
+    try {
+        const { docId } = req.body;
+        const appointments = await appointmentModel.find({ docId });
+        
+        res.json({ success: true, appointments });
+        
+    } catch (error) {
+        res.json({ success: false, message: error.message });
+    }
+}
+
+
+export { changeAvailability, doctorList, loginDoctor, appointmentsDoctor }
